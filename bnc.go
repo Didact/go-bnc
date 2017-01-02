43400dcbedd9ea89bb06f862f8b4844b0b04584f
Dakota Smith <dakotasmith@outlook.com>
2017-01-01 23:58:30 -0700

    added flags

--- bnc.go
+++ bnc.go
@@ -1,7 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+)
+
+// flags
+var (
+	prefix    = flag.String("prefix", "bnc-", "prefix for commands sent to the bnc instead of the server")
+	server    = flag.String("server", "", "server address to connect to")
+	localPort = flag.Int("localPort", 3434, "local port to listen on")
+	verbose   = flag.Bool("verbose", false, "verbose logging on/off")
 )
 
 func main() {
